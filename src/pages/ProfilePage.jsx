c165b6723830d05707ec5ce79b79ca4c5cb67377
rizwan <118995669+riz-33@users.noreply.github.com>
2025-02-22 16:18:26 +0500

    profile page

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,3 +1,4 @@
+import { UserOutlined } from "@ant-design/icons";
 import {
   Button,
   Card,
@@ -8,170 +9,142 @@ import {
   Row,
   Select,
   Typography,
+  message,
+  Avatar,
+  Space,
 } from "antd";
 import User from "../context/user";
 import { useContext, useEffect, useState } from "react";
-import {
-  doc,
-  getDoc,
-  collection,
-  db,
-  query,
-  orderBy,
-  onSnapshot,
-  updateDoc,
-} from "../config/firebase";
+import { doc, getDoc, db, updateDoc } from "../config/firebase";
 
-const { Option } = Select;
-
-const formItemLayout = {
-  labelCol: {
-    xs: {
-      span: 24,
-    },
-    sm: {
-      span: 6,
-    },
-  },
-  wrapperCol: {
-    xs: {
-      span: 24,
-    },
-    sm: {
-      span: 16,
-    },
-  },
-};
-const tailFormItemLayout = {
-  wrapperCol: {
-    xs: {
-      span: 24,
-      offset: 0,
-    },
-    sm: {
-      span: 16,
-      offset: 8,
-    },
-  },
-};
 const ProfilePage = () => {
+  const { Option } = Select;
   const user = useContext(User).user;
+  const [loading, setLoading] = useState(false);
+  const [form] = Form.useForm();
+
   useEffect(() => {
     const fetchData = async () => {
       const docRef = doc(db, "users", user.uid);
       const docSnap = await getDoc(docRef);
-
-      if (docSnap.exists()) {
-        console.log("Document data:", docSnap.data());
-      } else {
-        // docSnap.data() will be undefined in this case
-        console.log("No such document!");
-      }
     };
     fetchData();
   }, [user.uid]);
 
-  const [form] = Form.useForm();
-
   const onFinish = async (values) => {
+    setLoading(true); // Start loading
     const userRef = doc(db, "users", user.uid);
-    await updateDoc(userRef, {
-      username: values.username,
-      email: values.email,
-      number: values.number,
-      currency: values.currency,
-    });
-    console.log("Received values of form: ", values);
+    try {
+      await updateDoc(userRef, {
+        username: values.username,
+        email: values.email,
+        number: values.number,
+        currency: values.currency,
+      });
+      message.success("Profile updated successfully!");
+    } catch (error) {
+      message.error("Failed to update profile.", error);
+    } finally {
+      setLoading(false);
+    }
   };
+
   return (
-    <Row style={{ marginTop: "60px" }}>
+    <Row style={{ marginTop: "40px" }}>
       <Col xs={2} sm={4} md={4} lg={5} xl={6}></Col>
       <Col xs={20} sm={16} md={16} lg={14} xl={12}>
         <Card>
           <Typography
             style={{ fontSize: 20, textAlign: "center", marginBottom: 10 }}
           >
-            {" "}
             User Profile
           </Typography>
+
+          <Space
+            style={{
+              display: "flex",
+              alignItems: "center",
+              padding: 5,
+              marginBottom: 15,
+              cursor:"pointer",
+            }}
+            direction="vertical"
+            size={16}
+          >
+            <Avatar
+              // src={
+              //   "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/1200px-Google_%22G%22_logo.svg.png"
+              // }
+              size={150}
+              icon={<UserOutlined />}
+            />
+          </Space>
+
           <Form
-            {...formItemLayout}
             form={form}
             name="register"
             onFinish={onFinish}
-            style={{
-              paddingBlock: 20,
-            }}
+            layout="vertical"
             scrollToFirstError
           >
-            <Form.Item
-              name="username"
-              label="Username"
-              rules={[
-                {
-                  required: true,
-                  message: "Please input your Username!",
-                },
-              ]}
-            >
-              <Input defaultValue={user.username} />
-            </Form.Item>
-
-            <Form.Item
-              name="email"
-              label="E-mail"
-              rules={[
-                {
-                  type: "email",
-                  message: "The input is not valid E-mail!",
-                },
-                {
-                  required: true,
-                  message: "Please input your E-mail!",
-                },
-              ]}
-            >
-              <Input defaultValue={user.email} />
-            </Form.Item>
-
-            <Form.Item
-              name="number"
-              label="Phone Number"
-              rules={[
-                {
-                  required: true,
-                  message: "Please input your phone number!",
-                },
-              ]}
-            >
-              <Input defaultValue={user.number} />
-            </Form.Item>
-
-            <Form.Item
-              name="currency"
-              label="Currency"
-              rules={[
-                {
-                  required: true,
-                  message: "Please select Currency Type!",
-                },
-              ]}
-            >
-              <Select
-                placeholder="select your currency"
-                defaultValue={user.currency}
+            <div style={{ display: "flex", gap: "16px" }}>
+              <Form.Item
+                style={{ width: "100%" }}
+                name="username"
+                label="Username"
+                initialValue={user.username}
               >
-                <Option value="PKR">PKR &nbsp; &nbsp; Pakistani Rupee</Option>
-                <Option value="USD">USD &nbsp; &nbsp; US Dollar</Option>
-                <Option value="EUR">EUR &nbsp; &nbsp; Euro</Option>
-                <Option value="GBP">GBP &nbsp; &nbsp; Pound Sterling</Option>
-                <Option value="SAR">SAR &nbsp; &nbsp; Saudi Riyal </Option>
-              </Select>
-            </Form.Item>
+                <Input />
+              </Form.Item>
 
-            <Form.Item {...tailFormItemLayout}>
+              <Form.Item
+                style={{ width: "100%" }}
+                initialValue={user.email}
+                name="email"
+                label="E-mail"
+                rules={[
+                  {
+                    type: "email",
+                  },
+                ]}
+              >
+                <Input />
+              </Form.Item>
+            </div>
+
+            <div style={{ display: "flex", gap: "16px" }}>
+              <Form.Item
+                style={{ width: "100%" }}
+                initialValue={user.number}
+                name="number"
+                label="Phone Number"
+              >
+                <Input />
+              </Form.Item>
+
+              <Form.Item
+                style={{ width: "100%" }}
+                initialValue={user.currency}
+                name="currency"
+                label="Currency"
+              >
+                <Select placeholder="select your currency">
+                  <Option value="PKR">PKR &nbsp; &nbsp; Pakistani Rupee</Option>
+                  <Option value="USD">USD &nbsp; &nbsp; US Dollar</Option>
+                  <Option value="EUR">EUR &nbsp; &nbsp; Euro</Option>
+                  <Option value="GBP">GBP &nbsp; &nbsp; Pound Sterling</Option>
+                  <Option value="SAR">SAR &nbsp; &nbsp; Saudi Riyal </Option>
+                </Select>
+              </Form.Item>
+            </div>
+
+            <Form.Item
+              style={{ display: "flex", justifyContent: "center", margin: 12 }}
+            >
               <Flex gap="small">
                 <Button
+                  loading={loading}
+                  disabled={loading}
                   style={{ backgroundColor: "#1a237e" }}
                   type="primary"
                   htmlType="submit"
